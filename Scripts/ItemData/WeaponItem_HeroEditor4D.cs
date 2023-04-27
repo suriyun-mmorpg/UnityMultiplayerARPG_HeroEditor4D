5a5800e4b551df17a6d79119e18045567abbcd91
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-27 13:45:25 +0700

    feat: ignore some field/property from JSON serializing

--- Scripts/ItemData/WeaponItem_HeroEditor4D.cs
+++ Scripts/ItemData/WeaponItem_HeroEditor4D.cs
@@ -1,11 +1,14 @@
 ﻿using MultiplayerARPG.HeroEditor4D;
+using Newtonsoft.Json;
 
 namespace MultiplayerARPG
 {
     public partial class WeaponItem : IHeroEditorItem
     {
         [Category(11, "In-Scene Objects/Appearance (HeroEditor4D)")]
+        [JsonIgnore]
         public HeroEditorSpriteData spriteData;
+        [JsonIgnore]
         public HeroEditorSpriteData SpriteData { get { return spriteData; } }
     }
 }
